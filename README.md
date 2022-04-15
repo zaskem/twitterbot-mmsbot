c5da104ee5270c99f5d6528235d536e5d124c2b3
Matt Zaske <zaskem@mzonline.com>
2022-04-15 16:26:37 -0500

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,52 @@
+# Twitter "MMS MOA" Bot
+A [novelty bot](https://twitter.com/mmsmoabot) written in PHP to awareness- and exposure-retweet recent original tagged content (at random) related to the [MMSMOA conference](https://mmsmoa.com/).
+
+Original content is sourced from the [Twitter API v2 Search Recent Tweets](https://developer.twitter.com/en/docs/twitter-api/tweets/search/introduction) API endpoint via selective query parameters.
+
+This bot is not designed to like/retweet every original tweet it finds. It will instead select one at random from recent activity. The designed target is to catch every third or fourth tweet so as to be interesting enough (as a mechanism) but not simply a filtered stream of all matches (folks can do their own searches for that).
+
+## Requirements
+To run the bot code, the following libraries/accounts/things are required:
+
+* A bot/user account on Twitter for tweets;
+* A project and app configured on the [Twitter Developer Portal](https://developer.twitter.com/);
+* A manner by which you can generate an OAuth token and grant permission to the app for the bot account as necessary (such as Twurl); and
+* A host on which to run this code (not at a browsable path).
+
+### Twitter API
+Applying for access to the [Twitter Developer Portal](https://developer.twitter.com/) is outside the scope of this README. You will need to create a new Project and/or App for the Twitter bot and configure the App permissions to allow `Read and Write` access. You will obtain the API `consumer_key` and `consumer_secret` from the App's keys and tokens page, along with ability to generate a Bearer Token and OAuth 2.0 CLient ID/Secret.
+
+Assuming the account associated with the Developer Portal is _not_ the bot account, you will need to enable `3-Legged OAuth` for the App. This is required to generate a user access token and secret for an independent bot account.
+
+#### A note about generating user access tokens and secrets:
+This repo does not include a library/mechanism to address user access and callback for the bot app, which is ___required___ to generate a user access token and secret, and is generally a one-time action. It is recommended to use [Twurl](https://developer.twitter.com/en/docs/tutorials/using-twurl) for its simplicity. The following steps on a local WSL or Ubuntu instance (independent of the bot host if necessary) will generate the token and secret:
+
+1. `gem install twurl` (to install Twurl, also requires ruby)
+2. `twurl authorize --consumer-key key --consumer-secret secret` (with your Twitter App key/secret, follow prompts)
+3. `more ~/.twurlrc` (to obtain the bot account `token` and `secret` values)
+
+## Bot Configuration
+A single configuration file resides in the `config/` directory. An example version is provided, and to get started should be copied without the `.example` extension (e.g. `cp bot.php.example bot.php`)
+
+Edit the `bot.php` file as necessary for your configuration.
+
+## Bot Usage, Likes, Retweets, and Crontab Setup
+The bot process has been designed to run on an interval to search Twitter. The bot then evaluates recent original tweets, grabs a random (not previously retweeted) tweet, likes, and retweets the selection via the simple command:
+`php LikeAndRetweet.php`
+
+The bot is also designed to "self-meter" its own volume of retweets. It will adjust the "next retweet" interval based on the volume of tweets from the previous 1-2 hours. This basically means if tweet volume is low (e.g. 4 tweets in the last 2 hours) it would only retweet 1 time in about the same interval. However, if tweet volume is high (e.g. 10 tweets in the last 15 minutes), it would retweet once every 5 minutes.
+
+Cron should be used for production. A simple default crontab setting might look like this:
+```bash
+*/5 * * * * /path/to/php /path/to/LikeAndRetweet.php
+```
+The above will run the bot script every 5 minutes, a responsibly-appropriate interval for such a bot.
+
+## Troubleshooting and Tweet Posting
+This bot doesn't have a lot of moving parts, so there's not a lot to troubleshoot. There are two general points of failure:
+
+* Failure to obtain data from Twitter; and
+* Failure to post a like/retweet.
+
+## Contributors
+Developed as an opportunity to use Twitter's v2 API over a few lazy afternoons as a novelty bot/project by [@zaskem](https://github.com/zaskem). Public release was in advance of MMS (at MOA) 2022.
---
+++ TwitterAPIFunctions.php
@@ -0,0 +1,384 @@
+<?php
+  require_once(__DIR__ . '/config/bot.php');
+
+
+  /**
+   * getNonce($length = 11) - generate a nonce value for OAuth signatures
+   * 
+   * 100% stolen from the source at https://github.com/BaglerIT/OAuthSimple/blob/master/src/OAuthSimple.php and full credit goes to them! 
+   *
+   * @return string of a reasonably-unique nonce value
+   */
+  function getNonce($length = 11) {
+    $nonce_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    $result = '';
+    $cLength = strlen($nonce_chars);
+    for ($i = 0; $i < $length; $i++) {
+      $rnum = rand(0, $cLength - 1);
+      $result .= substr($nonce_chars, $rnum, 1);
+    }
+    return $result;
+  }
+
+
+  /**
+   * searchTweets(array $needles = null) - search for tweets
+   * 
+   * $needles: array of query parameters to override from the default set (set in `bot.php`), default null
+   *
+   * @return array of JSON-decoded search results, or boolean false if none/error
+   */
+  function searchTweets(array $needles = null) {
+    global $twitterBearerToken, $twitterSearchEndpoint, $twitterQueryDefaults;
+
+    // Add overridden search needles if provided
+    if (!is_null($needles)) {
+      $twitterQueryDefaults = array_merge($twitterQueryDefaults, $needles);
+    }
+
+    $flattenedArgs = '';
+    $argKeys = array_keys($twitterQueryDefaults);
+    $lastKey = end($argKeys);
+    foreach ($twitterQueryDefaults as $key=>$value) {
+      if ('query' == $key) {
+        $flattenedArgs .= $key . "=" . rawurlencode($value);
+      } else {
+        $flattenedArgs .= $key . "=" . $value;
+      }
+      if ($key != $lastKey) {
+        $flattenedArgs .= "&";
+      }
+    }
+    $searchEndpointWithArgs = $twitterSearchEndpoint . '?' . $flattenedArgs;
+
+    $curl_header = array("Authorization: Bearer $twitterBearerToken");
+
+    // Create/Submit cURL request
+    $curl_request = curl_init();
+    curl_setopt_array($curl_request, array(
+      CURLOPT_URL => $searchEndpointWithArgs,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_SSL_VERIFYPEER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+      CURLOPT_HTTPHEADER => $curl_header
+    ));
+
+    $json = curl_exec($curl_request);
+    curl_close($curl_request);
+
+    $results = json_decode($json, true);
+
+    if (array_key_exists("errors", $results)) {
+      return false;
+    } else if (0 == $results['meta']['result_count']) {
+      return false;
+    } else {
+      return $results;
+    }
+  }
+
+
+  /**
+   * likeTweet($tweetId) - retweet $tweetId
+   * 
+   * $tweetId: Twitter's unique id of the parent/source tweet 
+   *
+   * @return boolean of success or failure
+   */
+  function likeTweet($tweetId) {
+    global $twitterApiKey, $twitterApiKeySecret, $authToken, $authToken_secret, $twitterLikeEndpoint;
+    $timestamp = time();
+    $nonce = getNonce();
+
+    // Create OAuth Signature
+    $oauth_hash = 'oauth_consumer_key='.$twitterApiKey.'&';
+    $oauth_hash .= 'oauth_nonce='.$nonce.'&';
+    $oauth_hash .= 'oauth_signature_method=HMAC-SHA1&';
+    $oauth_hash .= 'oauth_timestamp='.$timestamp.'&';
+    $oauth_hash .= 'oauth_token='.$authToken.'&';
+    $oauth_hash .= 'oauth_version=1.0';
+
+    $base = 'POST&' . rawurlencode($twitterLikeEndpoint) . '&' .
+      rawurlencode($oauth_hash);
+    $key = rawurlencode($twitterApiKeySecret) . '&' . rawurlencode($authToken_secret);
+    $signature = base64_encode(hash_hmac('sha1', $base, $key, true));
+
+    // Create OAuth Header for cURL
+    $oauth_header = 'oauth_consumer_key="'.rawurlencode($twitterApiKey).'", ';
+    $oauth_header .= 'oauth_nonce="'.$nonce.'", ';
+    $oauth_header .= 'oauth_signature="'.rawurlencode($signature).'", ';
+    $oauth_header .= 'oauth_signature_method="'.rawurlencode('HMAC-SHA1').'", ';
+    $oauth_header .= 'oauth_timestamp="'.$timestamp.'", ';
+    $oauth_header .= 'oauth_token="'.rawurlencode($authToken).'", ';
+    $oauth_header .= 'oauth_version="1.0"';
+
+    $curl_header = array("Authorization: OAuth $oauth_header", 'Content-Type: application/json');
+    
+    // Create/Submit cURL request
+    $curl_request = curl_init();
+    curl_setopt_array($curl_request, array(
+      CURLOPT_URL => $twitterLikeEndpoint,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_SSL_VERIFYPEER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_POSTFIELDS =>'{
+        "tweet_id": "'.$tweetId.'"
+      }',
+      CURLOPT_HTTPHEADER => $curl_header,
+    ));
+
+    $json = curl_exec($curl_request);
+    curl_close($curl_request);
+
+    if (!array_key_exists("data", json_decode($json))) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+  /**
+   * unlikeTweet($tweetId) - delete the retweet of $tweetId
+   * 
+   * $tweetId: Twitter's unique id of the parent/source tweet 
+   *
+   * @return boolean of success or failure
+   */
+  function unlikeTweet($tweetId) {
+    global $twitterApiKey, $twitterApiKeySecret, $authToken, $authToken_secret, $twitterLikeEndpoint;
+    $timestamp = time();
+    $nonce = getNonce();
+    $unlikeTweetEndpoint = $twitterLikeEndpoint . '/' . $tweetId;
+
+    // Create OAuth Signature
+    $oauth_hash = 'oauth_consumer_key='.$twitterApiKey.'&';
+    $oauth_hash .= 'oauth_nonce='.$nonce.'&';
+    $oauth_hash .= 'oauth_signature_method=HMAC-SHA1&';
+    $oauth_hash .= 'oauth_timestamp='.$timestamp.'&';
+    $oauth_hash .= 'oauth_token='.$authToken.'&';
+    $oauth_hash .= 'oauth_version=1.0';
+
+    $base = 'DELETE&' . rawurlencode($unlikeTweetEndpoint) . '&' .
+      rawurlencode($oauth_hash);
+    $key = rawurlencode($twitterApiKeySecret) . '&' . rawurlencode($authToken_secret);
+    $signature = base64_encode(hash_hmac('sha1', $base, $key, true));
+
+    // Create OAuth Header for cURL
+    $oauth_header = 'oauth_consumer_key="'.rawurlencode($twitterApiKey).'", ';
+    $oauth_header .= 'oauth_nonce="'.$nonce.'", ';
+    $oauth_header .= 'oauth_signature="'.rawurlencode($signature).'", ';
+    $oauth_header .= 'oauth_signature_method="'.rawurlencode('HMAC-SHA1').'", ';
+    $oauth_header .= 'oauth_timestamp="'.$timestamp.'", ';
+    $oauth_header .= 'oauth_token="'.rawurlencode($authToken).'", ';
+    $oauth_header .= 'oauth_version="1.0"';
+
+    $curl_header = array("Authorization: OAuth $oauth_header");
+    
+    // Create/Submit cURL request
+    $curl_request = curl_init();
+    curl_setopt_array($curl_request, array(
+      CURLOPT_URL => $unlikeTweetEndpoint,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_SSL_VERIFYPEER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'DELETE',
+      CURLOPT_HTTPHEADER => $curl_header,
+    ));
+
+    $json = curl_exec($curl_request);
+    curl_close($curl_request);
+
+    if (!array_key_exists("data", json_decode($json))) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+  /**
+   * retweetContent($tweetId) - retweet $tweetId
+   * 
+   * $tweetId: Twitter's unique id of the parent/source tweet 
+   *
+   * @return boolean of success or failure
+   */
+  function retweetContent($tweetId) {
+    global $twitterApiKey, $twitterApiKeySecret, $authToken, $authToken_secret, $twitterRetweetEndpoint;
+    $timestamp = time();
+    $nonce = getNonce();
+
+    // Create OAuth Signature
+    $oauth_hash = 'oauth_consumer_key='.$twitterApiKey.'&';
+    $oauth_hash .= 'oauth_nonce='.$nonce.'&';
+    $oauth_hash .= 'oauth_signature_method=HMAC-SHA1&';
+    $oauth_hash .= 'oauth_timestamp='.$timestamp.'&';
+    $oauth_hash .= 'oauth_token='.$authToken.'&';
+    $oauth_hash .= 'oauth_version=1.0';
+
+    $base = 'POST&' . rawurlencode($twitterRetweetEndpoint) . '&' .
+      rawurlencode($oauth_hash);
+    $key = rawurlencode($twitterApiKeySecret) . '&' . rawurlencode($authToken_secret);
+    $signature = base64_encode(hash_hmac('sha1', $base, $key, true));
+
+    // Create OAuth Header for cURL
+    $oauth_header = 'oauth_consumer_key="'.rawurlencode($twitterApiKey).'", ';
+    $oauth_header .= 'oauth_nonce="'.$nonce.'", ';
+    $oauth_header .= 'oauth_signature="'.rawurlencode($signature).'", ';
+    $oauth_header .= 'oauth_signature_method="'.rawurlencode('HMAC-SHA1').'", ';
+    $oauth_header .= 'oauth_timestamp="'.$timestamp.'", ';
+    $oauth_header .= 'oauth_token="'.rawurlencode($authToken).'", ';
+    $oauth_header .= 'oauth_version="1.0"';
+
+    $curl_header = array("Authorization: OAuth $oauth_header", 'Content-Type: application/json');
+    
+    // Create/Submit cURL request
+    $curl_request = curl_init();
+    curl_setopt_array($curl_request, array(
+      CURLOPT_URL => $twitterRetweetEndpoint,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_SSL_VERIFYPEER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_POSTFIELDS =>'{
+        "tweet_id": "'.$tweetId.'"
+      }',
+      CURLOPT_HTTPHEADER => $curl_header,
+    ));
+
+    $json = curl_exec($curl_request);
+    curl_close($curl_request);
+
+    if (!array_key_exists("data", json_decode($json))) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+  /**
+   * deleteRetweet($tweetId) - delete the retweet of $tweetId
+   * 
+   * $tweetId: Twitter's unique id of the parent/source tweet 
+   *
+   * @return boolean of success or failure
+   */
+  function deleteRetweet($tweetId) {
+    global $twitterApiKey, $twitterApiKeySecret, $authToken, $authToken_secret, $twitterRetweetEndpoint;
+    $timestamp = time();
+    $nonce = getNonce();
+    $deleteTweetEndpoint = $twitterRetweetEndpoint . '/' . $tweetId;
+
+    // Create OAuth Signature
+    $oauth_hash = 'oauth_consumer_key='.$twitterApiKey.'&';
+    $oauth_hash .= 'oauth_nonce='.$nonce.'&';
+    $oauth_hash .= 'oauth_signature_method=HMAC-SHA1&';
+    $oauth_hash .= 'oauth_timestamp='.$timestamp.'&';
+    $oauth_hash .= 'oauth_token='.$authToken.'&';
+    $oauth_hash .= 'oauth_version=1.0';
+
+    $base = 'DELETE&' . rawurlencode($deleteTweetEndpoint) . '&' .
+      rawurlencode($oauth_hash);
+    $key = rawurlencode($twitterApiKeySecret) . '&' . rawurlencode($authToken_secret);
+    $signature = base64_encode(hash_hmac('sha1', $base, $key, true));
+
+    // Create OAuth Header for cURL
+    $oauth_header = 'oauth_consumer_key="'.rawurlencode($twitterApiKey).'", ';
+    $oauth_header .= 'oauth_nonce="'.$nonce.'", ';
+    $oauth_header .= 'oauth_signature="'.rawurlencode($signature).'", ';
+    $oauth_header .= 'oauth_signature_method="'.rawurlencode('HMAC-SHA1').'", ';
+    $oauth_header .= 'oauth_timestamp="'.$timestamp.'", ';
+    $oauth_header .= 'oauth_token="'.rawurlencode($authToken).'", ';
+    $oauth_header .= 'oauth_version="1.0"';
+
+    $curl_header = array("Authorization: OAuth $oauth_header");
+    
+    // Create/Submit cURL request
+    $curl_request = curl_init();
+    curl_setopt_array($curl_request, array(
+      CURLOPT_URL => $deleteTweetEndpoint,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_SSL_VERIFYPEER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'DELETE',
+      CURLOPT_HTTPHEADER => $curl_header,
+    ));
+
+    $json = curl_exec($curl_request);
+    curl_close($curl_request);
+
+    if (!array_key_exists("data", json_decode($json))) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+  /**
+   * lookupUser($twitterUsername) - find the user ID of the $twitterUsername user
+   * 
+   * This call has been deliberately designed as a convenience to only return the first match.
+   * The function is not used in runtime of the bot; use at your own risk/peril.
+   * 
+   * @return string of the Twitter user ID
+   * @return boolean false for mismatch/error
+   */
+  function lookupUser($twitterUsername) {
+    global $twitterBearerToken, $twitterUserIdLookupEndpoint;
+
+    $curl_header = array('Authorization: Bearer '.$twitterBearerToken);
+    // Modify the endpoint URL with argument provided
+    $twitterUserIdLookupEndpointWithArgs = $twitterUserIdLookupEndpoint . '?usernames=' . $twitterUsername;
+
+    // Create/Submit cURL request
+    $curl_request = curl_init();
+    curl_setopt_array($curl_request, array(
+      CURLOPT_URL => $twitterUserIdLookupEndpointWithArgs,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_SSL_VERIFYPEER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+      CURLOPT_HTTPHEADER => $curl_header,
+    ));
+
+    $json = curl_exec($curl_request);
+    curl_close($curl_request);
+
+    $result = json_decode($json, true);
+    if (array_key_exists("errors", $result)) {
+      return false;
+    } else {
+      return $result['data'][0]['id'];
+    }
+  }
+
+?>
